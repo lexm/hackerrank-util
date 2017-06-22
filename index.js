fb62f108836fecccfe0982aea66f2474b4959927
Lex Myers <lex.myers@gmail.com>
2017-06-22 14:00:20 -0700

    add makeCodeDir function

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const exec = require('child_process').exec;
 const home = process.env.HOME;
 const repo = process.env.HACKERRANK_REPO || home + '/hackerrank-code/';
 
+const makeCodeDir = function(repo, pathArray) {
+  let codeDir = repo;
+  while(true) {
+    try {
+      fs.statSync(codeDir);
+    } catch(e) {
+      fs.mkdirSync(codeDir);
+    }
+    if(!pathArray.length) break;
+    codeDir += pathArray.splice(0, 1)[0] + '/';
+  }
+  return codeDir;
+};
+
 program
   .version('0.0.1')
   .arguments('<filename>')
