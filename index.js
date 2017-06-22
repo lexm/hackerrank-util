75d357e33db6e080afa8624da6471c2cb4db636b
Lex Myers <lex.myers@gmail.com>
2017-06-22 14:40:53 -0700

    fix variable names to allow for destructuring

--- index.js
+++ index.js
@@ -21,31 +21,35 @@ const makeCodeDir = function(repo, pathArray) {
 
 program
   .version('0.0.1')
-  .arguments('<filename>')
+  .arguments('<downloadName>')
     .option('-a, --add', 'add solution to git without performing commit')
     .option('-c, --commit', 'add and commit solution with git (default)')
     .option('-n, --no_add', 'refrain from running git add')
-    .action(function(filename) {
+    .action(function(downloadName) {
       var filePath;
-      if(filename.indexOf('/') === -1) {
-        filePath = process.cwd() + '/' + filename;
+      if(downloadName.indexOf('/') === -1) {
+        filePath = process.cwd() + '/' + downloadName;
       } else {
-        filePath = filename;
+        filePath = downloadName;
       }
       console.log(filePath);
       let scriptData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-      let { pathArray, message, solutionName, allCode} = scriptData;
-      let fullPath = makeCodeDir(repo, pathArray);
-      fs.writeFile(fullPath + solutionName, allCode, function(err){
-        if(err) throw err;
-        console.log('File ' + solutionName + ' written');
+      let { pathArray, message, filename, allCode} = scriptData;
+      console.log(`pathArray: ${pathArray}`);
+      console.log(`message: ${message}`);
+      console.log(`filename: ${filename}`);
+      console.log(`allCode: ${allCode}`);
+      // let fullPath = makeCodeDir(repo, pathArray);
+      // fs.writeFile(fullPath + filename, allCode, function(err){
+      //   if(err) throw err;
+        // console.log('File ' + filename + ' written');
         // exec('cd ' + fullPath + ';git add ' + filename, function(error) {
         //   if(error) console.error(error);
         //   exec('cd ' + fullPath + ';git commit -m "' + message + '"', function(error) {
         //     if(error) console.error(error);
         //   });
         // });
-      });
+      // });
       if(program.add) console.log('add option');
       if(program.commit) console.log('commit option');
       if(program.no_add) console.log('no_add option');
