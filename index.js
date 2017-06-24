3b4529b2164a77c69c4a6722b93538c194af8b28
Lex Myers <lex.myers@gmail.com>
2017-06-23 17:19:26 -0700

    moved makeCodeDir to makedir.js file

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const fs = require('fs');
 const exec = require('child_process').exec;
 const home = process.env.HOME;
 const repo = process.env.HACKERRANK_REPO || home + '/hackerrank-code/';
+const makeCodeDir = require(__dirname + '/makedir').makeCodeDir;
 
-const makeCodeDir = function(repo, pathArray) {
-  let codeDir = repo;
-  while(true) {
-    try {
-      fs.statSync(codeDir);
-    } catch(e) {
-      fs.mkdirSync(codeDir);
-    }
-    if(!pathArray.length) break;
-    codeDir += pathArray.splice(0, 1)[0] + '/';
-  }
-  return codeDir;
-};
+// const makeCodeDir = function(repo, pathArray) {
+//   let codeDir = repo;
+//   while(true) {
+//     try {
+//       fs.statSync(codeDir);
+//     } catch(e) {
+//       fs.mkdirSync(codeDir);
+//     }
+//     if(!pathArray.length) break;
+//     codeDir += pathArray.splice(0, 1)[0] + '/';
+//   }
+//   return codeDir;
+// };
 
 program
   .version('0.0.1')
---
+++ makedir.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.makeCodeDir = function(repo, pathArray) {
+  let codeDir = repo;
+  while(true) {
+    try {
+      fs.statSync(codeDir);
+    } catch(e) {
+      fs.mkdirSync(codeDir);
+    }
+    if(!pathArray.length) break;
+    codeDir += pathArray.splice(0, 1)[0] + '/';
+  }
+  return codeDir;
+};
