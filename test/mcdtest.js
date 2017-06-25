1fe7b7d12af1dcc0be32bc54fa239d58785f7bd5
Lex Myers <lex.myers@gmail.com>
2017-06-25 16:54:03 -0700

    corrected before code; added mock.restore to run after

--- test/mcdtest.js
+++ test/mcdtest.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const expect = require('chai').expect;
 const mcd = require(__dirname + '/../makedir.js');
 const makeCodeDir = mcd.makeCodeDir;
@@ -7,7 +8,7 @@ const mock = require('mock-fs');
 
 
 describe('testing makeCodeDir', function() {
-  after(function(done) {
+  before(function(done) {
     mock({
       'hackerrank-repo': {
         '.git': {}
@@ -15,8 +16,12 @@ describe('testing makeCodeDir', function() {
     });
     done();
   });
+  after(function(done) {
+    mock.restore();
+    done();
+  })
   it('should be able to create new directories', function() {
     var newDir = makeCodeDir('./hackerrank-repo/', ['Python', 'Strings']);
     expect(newDir).to.equal('./hackerrank-repo/Python/Strings/');
-  })
-})
+  });
+});
