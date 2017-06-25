4d3246b02e6f91d86908dc5e1a44aab6c2af8d16
Lex Myers <lex.myers@gmail.com>
2017-06-25 14:21:51 -0700

    first try at crete new dir

--- test/mcdtest.js
+++ test/mcdtest.js
@@ -3,3 +3,20 @@
 const expect = require('chai').expect;
 const mcd = require(__dirname + '/../makedir.js');
 const makeCodeDir = mcd.makeCodeDir;
+const mock = require('mock-fs');
+
+
+describe('testing makeCodeDir', function() {
+  after(function(done) {
+    mock({
+      'hackerrank-repo': {
+        '.git': {}
+      }
+    });
+    done();
+  });
+  it('should be able to create new directories', function() {
+    var newDir = makeCodeDir('./hackerrank-repo', ['Python', 'Strings']);
+    expect(newDir).to.equal(process.cwd() + '/Python/Strings');
+  })
+})
