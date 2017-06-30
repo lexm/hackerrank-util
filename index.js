c4c84396a7690f8be0205fb527c16d89efc0ffcc
Lex Myers <lex.myers@gmail.com>
2017-06-30 16:06:56 -0700

    removed old code

--- index.js
+++ index.js
@@ -1,54 +1,11 @@
 #!/usr/bin/env node
 const program = require('commander');
-// const fs = require('fs');
-// const exec = require('child_process').exec;
-// const home = process.env.HOME;
-// const repo = process.env.HACKERRANK_REPO || home + '/hackerrank-code/';
-// const makeCodeDir = require(__dirname + '/lib/makedir').makeCodeDir;
-// const fillPath = require(__dirname + '/lib/fillpath').fillPath;
 const {
   commitCode,
   addCode,
   writeCodeFile,
   getScriptData
 } = require(__dirname + '/lib/addcode')
-
-// const commitCode = function(path, message) {
-//   exec('cd ' + path + ';git commit -m "' + message + '"', function(error) {
-//     if(error) {
-//       console.error(error)
-//     } else {
-//       console.log('committed: ' + message);
-//     }
-//   });
-// }
-//
-// const addCode = function(scriptData, callback) {
-//   exec('cd ' + scriptData.fullPath + ';git add ' + scriptData.filename, function(error) {
-//     if(error) {
-//       console.error(error);
-//     } else {
-//       console.log('added: ' + scriptData.filename);
-//       if(!scriptData.add) {
-//         callback(scriptData.fullPath, scriptData.message)
-//       }
-//     }
-//   });
-// }
-//
-// const writeCodeFile = function(scriptData, callback) {
-//   fs.writeFile(scriptData.fullPath + scriptData.filename, scriptData.allCode, function(err){
-//     if(err) throw err;
-//     console.log('File ' + scriptData.filename + ' written');
-//     callback();
-//   });
-// }
-//
-// const getScriptData = function(downloadName) {
-//   let scriptData = JSON.parse(fs.readFileSync(fillPath(downloadName), 'utf8'));
-//   scriptData.fullPath = makeCodeDir(repo, scriptData.pathArray);
-//   return scriptData;
-// }
 
 program
   .version('0.0.1')
