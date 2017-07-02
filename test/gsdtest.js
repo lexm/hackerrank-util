329bfa12db9924c67c81ba3f85072f702b7aaa54
Lex Myers <lex.myers@gmail.com>
2017-07-01 20:10:37 -0700

    set up mock-fs

--- test/gsdtest.js
+++ test/gsdtest.js
@@ -5,3 +5,17 @@ const expect = require('chai').expect;
 const addCode = require(__dirname + '/../lib/addcode.js');
 const makeCodeDir = addCode.getScriptData;
 const mock = require('mock-fs');
+
+describe('getScriptData', function() {
+  before(function(done) {
+    mock({
+      'downloads': {
+      }
+    });
+    done();
+  });
+  after(function(done) {
+    mock.restore();
+    done();
+  })
+});
