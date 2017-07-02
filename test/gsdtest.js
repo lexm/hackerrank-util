291690269984b10962443ce0dbaeac61b716cae4
Lex Myers <lex.myers@gmail.com>
2017-07-01 21:17:07 -0700

    corrected error message to expect

--- test/gsdtest.js
+++ test/gsdtest.js
@@ -39,7 +39,7 @@ describe('getScriptData', function() {
     it('should throw an error', function(done) {
       expect(function() {
         getScriptData('downloads/file01.json');
-      }).to.throw('SyntaxError');
+      }).to.throw('Unexpected token');
       done();
     })
   });
