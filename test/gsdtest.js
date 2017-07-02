e7123ead23dffdaceeebbc4fb4c2370072f9718a
Lex Myers <lex.myers@gmail.com>
2017-07-01 21:07:57 -0700

    reordered test files

--- test/gsdtest.js
+++ test/gsdtest.js
@@ -9,17 +9,17 @@ const mock = require('mock-fs');
 describe('getScriptData', function() {
   before(function(done) {
     mock({
+      'file01.json': 'I am not a JSON file!',
+      'file02.json': '{"name": "unknown field","pi": 3.14, "not_hyk": true}',
       'downloads': {
-        'file01.json': '{"progName":"Compute the Average",' +
+        'file03.json': '{"progName":"Compute the Average",' +
         '"pathArray":["Linux_Shell","Bash"],' +
         '"message":"Solution to Linux Shell > Bash > Compute the Average",' +
         '"filename":"bash-tutorials---compute-the-average.sh",' +
         '"allCode":"read N\nTOTAL=0\nCOUNT=$N\nwhile [ $COUNT -gt 0 ];' +
         ' do\n  read INT\n  let TOTAL+=$INT\n  let COUNT-=1\ndone\n' +
         'printf \"%.3f\\n\" $(echo \" $TOTAL / $N \" | bc -l)\n"}'
-      },
-      'file02.json': 'I am not a JSON file!',
-      'file03.json': '{"name": "unknown field","pi": 3.14, "not_hyk": true}'
+      }
     });
     done();
   });
@@ -35,4 +35,5 @@ describe('getScriptData', function() {
       done();
     });
   });
+  describe('when download file is')
 });
