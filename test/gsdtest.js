71319cc6171971fb6a22ff688e541def9bf37081
Lex Myers <lex.myers@gmail.com>
2017-07-03 15:20:40 -0700

    added test for scriptData.pathArray

--- test/gsdtest.js
+++ test/gsdtest.js
@@ -5,12 +5,14 @@ const expect = require('chai').expect;
 const addCode = require(__dirname + '/../lib/addcode.js');
 const getScriptData = addCode.getScriptData;
 const mock = require('mock-fs');
+
 const file03Data = {
-  "progName":"Compute the Average",
-  "pathArray":["Linux_Shell","Bash"],
-  "message":"Solution to Linux Shell > Bash > Compute the Average",
-  "filename":"bash-tutorials---compute-the-average.sh",
-  "allCode":"read N\nTOTAL=0\nCOUNT=$N\nwhile [ $COUNT -gt 0 ]; do\n  read INT\n  let TOTAL+=$INT\n  let COUNT-=1\ndone\nprintf \"%.3f\\n\" $(echo \" $TOTAL / $N \" | bc -l)\n"}
+  "progName": "Compute the Average",
+  "pathArray": [ 'Linux_Shell', 'Bash' ],
+  "message": "Solution to Linux Shell > Bash > Compute the Average",
+  "filename": "bash-tutorials---compute-the-average.sh",
+  "allCode": "read N\nTOTAL=0\nCOUNT=$N\nwhile [ $COUNT -gt 0 ]; do\n  read INT\n  let TOTAL+=$INT\n  let COUNT-=1\ndone\nprintf \"%.3f\\n\" $(echo \" $TOTAL / $N \" | bc -l)\n"
+}
 
 describe('getScriptData', function() {
   before(function(done) {
@@ -59,11 +61,25 @@ describe('getScriptData', function() {
       }).to.not.throw();
       done();
     });
-    it('should have a correct values for scriptData', function(done) {
-      expect(scriptData.progName).to.be.a('string');
-      expect(scriptData.progName).to.equal('Compute the Average');
-      expect(scriptData.pathArray).to.be.an.instanceOf(Array);
-      done();
-    })
+    describe('scriptData', function() {
+      it('should have correct type/value for scriptData.progName', function(done) {
+        expect(scriptData.progName).to.be.a('string');
+        expect(scriptData.progName).to.equal('Compute the Average');
+        done();
+      });
+      it('should have correct type/value for scriptData.pathArray', function(done) {
+        expect(scriptData.pathArray).to.be.an.instanceOf(Array);
+        expect(scriptData.pathArray.length).to.equal(2);
+        expect(scriptData.pathArray[0]).to.equal('Linux_Shell');
+        expect(scriptData.pathArray[1]).to.equal('Bash');
+        done();
+      });
+      // it('should have correct type/value for scriptData.progName', function(done) {
+      // });
+      // it('should have correct type/value for scriptData.progName', function(done) {
+      // });
+      // it('should have correct type/value for scriptData.progName', function(done) {
+      // });
+    });
   });
 });
