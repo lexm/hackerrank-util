a919296928387e6389e4ffe5104b116acd9db4eb
Lex Myers <lex.myers@gmail.com>
2017-07-02 17:49:52 -0700

    attempt to fix JSON

--- test/gsdtest.js
+++ test/gsdtest.js
@@ -12,7 +12,7 @@ describe('getScriptData', function() {
       'downloads': {
         'file01.json': 'I am not a JSON file!',
         'file02.json': '{"name": "unknown field","pi": 3.14, "not_hyk": true}',
-        'file03.json': '{"progName":"Compute the Average","pathArray":["Linux_Shell","Bash"],"message":"Solution to Linux Shell > Bash > Compute the Average","filename":"bash-tutorials---compute-the-average.sh","allCode":"read N\nTOTAL=0\nCOUNT=$N\nwhile [ $COUNT -gt 0 ]; do\n  read INT\n  let TOTAL+=$INT\n  let COUNT-=1\ndone\nprintf \"%.3f\\n\" $(echo \" $TOTAL / $N \" | bc -l)\n"}'
+        'file03.json': '{"progName":"Compute the Average","pathArray":["Linux_Shell","Bash"],"message":"Solution to Linux Shell > Bash > Compute the Average","filename":"bash-tutorials---compute-the-average.sh","allCode":"read N\\nTOTAL=0\\nCOUNT=$N\\nwhile [ $COUNT -gt 0 ]; do\\n  read INT\\n  let TOTAL+=$INT\\n  let COUNT-=1\\ndone\\nprintf \\"%.3f\\n\\" \$(echo \" \$TOTAL / \$N \" | bc -l)\\n"}'
       }
     });
     done();
