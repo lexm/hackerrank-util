6c4cff52f37ea5fd4e8983c75425b861a6319b6d
Lex Myers <lex.myers@gmail.com>
2017-07-03 19:04:25 -0700

    require mock-fs

--- test/wcftest.js
+++ test/wcftest.js
@@ -4,3 +4,4 @@ const fs = require('fs');
 const expect = require('chai').expect;
 const addCode = require(__dirname + '/../lib/addcode.js');
 const writeCodeFile = addCode.writeCodeFile;
+const mock = require('mock-fs');
