66edb3a81384b19613b6a61cdd63b0aafb31c191
Lex Myers <lex.myers@gmail.com>
2017-07-04 18:26:09 -0700

    added test data

--- test/wcftest.js
+++ test/wcftest.js
@@ -6,6 +6,14 @@ const addCode = require(__dirname + '/../lib/addcode.js');
 const writeCodeFile = addCode.writeCodeFile;
 const mock = require('mock-fs');
 
+const testData01 = {
+  "progName": "Compute the Average",
+  "pathArray": [ 'Linux_Shell', 'Bash' ],
+  "message": "Solution to Linux Shell > Bash > Compute the Average",
+  "filename": "bash-tutorials---compute-the-average.sh",
+  "allCode": "read N\nTOTAL=0\nCOUNT=$N\nwhile [ $COUNT -gt 0 ]; do\n  read INT\n  let TOTAL+=$INT\n  let COUNT-=1\ndone\nprintf \"%.3f\\n\" $(echo \" $TOTAL / $N \" | bc -l)\n"
+}
+
 describe('writeCodeFile', function() {
   before(function(done) {
     mock({
