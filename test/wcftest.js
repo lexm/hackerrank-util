fc1664df5f0df38c20b0cad934691f99bf0badc9
Lex Myers <lex.myers@gmail.com>
2017-07-04 18:29:28 -0700

    added fullPath to test data

--- test/wcftest.js
+++ test/wcftest.js
@@ -11,6 +11,7 @@ const testData01 = {
   "pathArray": [ 'Linux_Shell', 'Bash' ],
   "message": "Solution to Linux Shell > Bash > Compute the Average",
   "filename": "bash-tutorials---compute-the-average.sh",
+  "fullPath": repo + "Linux_Shell/Bash",
   "allCode": "read N\nTOTAL=0\nCOUNT=$N\nwhile [ $COUNT -gt 0 ]; do\n  read INT\n  let TOTAL+=$INT\n  let COUNT-=1\ndone\nprintf \"%.3f\\n\" $(echo \" $TOTAL / $N \" | bc -l)\n"
 }
 
