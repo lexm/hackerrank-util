d2b82422a4237f295bafa469476b16600be77139
Lex Myers <lex.myers@gmail.com>
2017-07-04 20:29:58 -0700

    added check for file data

--- test/wcftest.js
+++ test/wcftest.js
@@ -49,6 +49,7 @@ describe('writeCodeFile', function() {
       fs.readFile('./repo/Linux_Shell/Bash/bash-tutorials---compute-the-average.sh', 'utf8', function(err, data) {
         expect(err).to.be.null;
         console.log('data: ', data);
+        expect(data).to.equal(testData01.allCode);
         done();
       })
     });
