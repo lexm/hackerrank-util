6f37bfe81bed6f23fe9a74ee83e5e8a4e084ecdf
Lex Myers <lex.myers@gmail.com>
2017-07-04 18:54:16 -0700

    linted; renamed repo variable to check for conflict

--- test/wcftest.js
+++ test/wcftest.js
@@ -5,14 +5,14 @@ const expect = require('chai').expect;
 const addCode = require(__dirname + '/../lib/addcode.js');
 const writeCodeFile = addCode.writeCodeFile;
 const mock = require('mock-fs');
-const repo = './repo/'
+const repo1 = './repo/'
 
 const testData01 = {
   "progName": "Compute the Average",
   "pathArray": [ 'Linux_Shell', 'Bash' ],
   "message": "Solution to Linux Shell > Bash > Compute the Average",
   "filename": "bash-tutorials---compute-the-average.sh",
-  "fullPath": repo + "Linux_Shell/Bash",
+  "fullPath": repo1 + "Linux_Shell/Bash/",
   "allCode": "read N\nTOTAL=0\nCOUNT=$N\nwhile [ $COUNT -gt 0 ]; do\n  read INT\n  let TOTAL+=$INT\n  let COUNT-=1\ndone\nprintf \"%.3f\\n\" $(echo \" $TOTAL / $N \" | bc -l)\n"
 }
 
@@ -33,8 +33,8 @@ describe('writeCodeFile', function() {
         fs.access('./repo/Linux_Shell/', function(err) {
           expect(err).to.be.null;
           done();
-        })
-      })
-    })
+        });
+      });
+    });
   });
 });
