dab2b2574cfeffd55a732614d652c61af2600f35
Lex Myers <lex.myers@gmail.com>
2017-07-18 16:51:11 -0700

    new test for unwritten addPrefix function: scaffold

---
+++ test/addpretest.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+const addCode = require(__dirname + '/../lib/addcode.js');
+const addPrefix = addCode.addPrefix;
+
+const baseMsg = 'Linux Shell > Bash > Compute the Average';
+
+describe('addPrefix', function() {
+})
