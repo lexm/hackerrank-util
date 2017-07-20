b9eadc05cf70bb8e87f58d15bec285fe7864132c
Lex Myers <lex.myers@gmail.com>
2017-07-19 19:43:11 -0700

    changed index.js to add support for try option

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ program
   .option('-a, --add', 'add solution to git without performing commit')
   .option('-c, --commit', 'add and commit solution with git (default)')
   .option('-n, --no_add', 'refrain from running git add')
-  .option('-t, --try, <trynum>', 'attempt <number>')
+  .option('-t, --try, <tryNum>', 'attempt <number>')
   .action(function(downloadName) {
-    const scriptData = getScriptData(downloadName);
+    const scriptData = getScriptData(downloadName, program.tryNum);
     writeCodeFile(scriptData, function() {
       if(!scriptData.no_add) {
         addCode(scriptData, commitCode);
