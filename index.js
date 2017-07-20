3064d54bec0d8cb1f12cf022d2677b99354276f3
Lex Myers <lex.myers@gmail.com>
2017-07-20 10:44:56 -0700

    added parseInt to .option()

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const {
   writeCodeFile,
   getScriptData
 } = require(__dirname + '/lib/addcode')
-var tryNum;
 
 program
   .version('0.0.1')
@@ -14,14 +13,9 @@ program
   .option('-a, --add', 'add solution to git without performing commit')
   .option('-c, --commit', 'add and commit solution with git (default)')
   .option('-n, --no_add', 'refrain from running git add')
-  .option('-t, --try <trynum>', 'attempt <number>')
+  .option('-t, --try <trynum>', 'attempt <number>', parseInt)
   .action(function(downloadName) {
-    if(program.try) {
-      tryNum = program.trynum;
-    } else {
-      tryNum = 0
-    }
-    const scriptData = getScriptData(downloadName, tryNum);
+    const scriptData = getScriptData(downloadName, program.trynum);
     writeCodeFile(scriptData, function() {
       if(!scriptData.no_add) {
         addCode(scriptData, commitCode);
